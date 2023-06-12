8641b963fff010cf41393eaa91ddef18fc46d258
anton083 <periareion05@gmail.com>
2023-06-12 21:19:01 +0200

    some changes

--- src/filter.jl
+++ src/filter.jl
@@ -35,12 +35,16 @@ struct Read
     idx::Int64
 end
 
+function max_in_columns(matrix::Array{Int, 2})
+    return mapslices(maximum, matrix, dims=1)
+end
+
 function chunked_filter_fasta(
     reference_path::String,
     dataset_path::String;
     pident::Float64 = 0.7,
     k::Integer = 7,
-    subref_length::Integer = 2048,
+    subref_length::Integer = 2000,
     read_chunk_size::Integer = 1000,
 )
 
@@ -48,14 +52,18 @@ function chunked_filter_fasta(
     ref_subranges = sequence_subranges.(length.(ref_seqs), subref_length, 100)
     subrefs = reduce(vcat, subseqs.(ref_seqs, ref_subranges))
 
+    score_threshold, _ = estimate_score_threshold(subrefs[1], pident, k = k, sample_count = 1000)
+
     reference_bin_matrix = reference_kmer_matrix(subrefs, 7)
     
-    read_bin_matrix = zeros(UInt16, (4^k, read_chunk_size))
+    read_bin_matrix = zeros(Float16, (4^k, read_chunk_size))
     read_chunk = StructVector{Read}(undef, read_chunk_size)
+
+    matrix_product = zeros(Int, (length(subrefs), read_chunk_size))
 
     reader = FASTAReader(open(dataset_path), copy=false)
     read_count_total = 0
-
+    hits = 0
     while !eof(reader)
         for (i, record) in enumerate(reader)
             read_count_total += 1
@@ -65,8 +73,9 @@ function chunked_filter_fasta(
             i == read_chunk_size && break
         end
         read_kmer_matrix!(read_bin_matrix, read_chunk.seq, k)
-        reference_bin_matrix * read_bin_matrix
-        println(read_count_total)
+        mul!(matrix_product, reference_bin_matrix, read_bin_matrix)
+        hits += count(score > score_threshold, max_in_columns(matrix_product))
+        
         #=if read_count_total % read_chunk_size == 0
         end=#
 
