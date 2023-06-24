efccaa6f166076dcfb7145297bec5291fdb3e6e2
anton083 <periareion05@gmail.com>
2023-06-24 02:38:05 +0200

    Testing fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using ReadFilter
 using Test
 
 @testset "ReadFilter.jl" begin
-    @testset "io.jl" begin
+    @testset "utils.jl" begin
         @test get_subranges(10, 5, 2) == [1:5, 4:8, 6:10]
         @test get_subranges(10, 10, 2) == [1:10]
     end
