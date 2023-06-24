edcf6d5dece1efcedc7a9a942686d4dc505ca462
anton083 <periareion05@gmail.com>
2023-06-24 11:53:19 +0200

    fixed typo

--- src/API.jl
+++ src/API.jl
@@ -37,7 +37,7 @@ function find_reads_gpu(
         scores_d = subref_kmer_matrix_d * reads_kmer_matrix_d
 
         indices_of_matches = get_indices_of_matches(scores_d, score_thresholds_d)
-        indices_of_matches .+= read_count - read_co8nt % read_chunk_size#((read_count - 1) % read_chunk_size + 1)
+        indices_of_matches .+= read_count - read_count % read_chunk_size#((read_count - 1) % read_chunk_size + 1)
 
         # TODO: add check for homopolymers
 
