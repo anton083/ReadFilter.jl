4071dcbb5bcfbabb26bf303c1b64671d2153d317
anton083 <periareion05@gmail.com>
2023-06-26 17:26:10 +0200

    fix

--- src/reads.jl
+++ src/reads.jl
@@ -37,7 +37,7 @@ function get_matches(
     matches = Vector{Match}(undef, n)
     for (i, (read, subref, score)) in enumerate(zip(reads, subrefs, kmer_count_scores))
         matches[i] = Match(read, subref, score, missing)
-        alignment_score, a1, a2 = SWG_align(read.seq, get_sequence(subref), params)
+        alignment_score, a1, a2 = SWG_align(read.seq, get_sequence(subref), 1, 1)
         println(score)
         println(alignment_score)
         print_alignment(a1, a2)
